59b4799e266950c88b18d9449bcdb77309babd6e
Jeremy Ward <exochos@gmail.com>
2021-10-22 15:45:01 -0700

    Add files via upload

---
+++ ArrayListTest.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+}
--- SortedArrayList.java
+++ SortedArrayList.java
@@ -1,4 +1,70 @@
 package com.company;
 
-public abstract class SortedArrayList implements SortedArrayListInterface {
+import java.util.Iterator;
+
+public abstract class SortedArrayList<E extends Comparable<E>> implements SortedArrayListInterface {
+    ArrayList<E> arrayList;
+
+    public SortedArrayList() {
+        arrayList = new ArrayList<>();
+    }
+
+    public SortedArrayList(int intitialCapacity) {
+        arrayList = new ArrayList<>(intitialCapacity);
+    }
+
+    @Override
+    public int size() {
+        return arrayList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean contains(Object value) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(Object value) {
+        return 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public Object[] get(Object value, Object[] template) {
+        return new Object[0];
+    }
+
+    @Override
+    public void add(Object value) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray(Object[] template) {
+        return new Object[0];
+    }
 }
